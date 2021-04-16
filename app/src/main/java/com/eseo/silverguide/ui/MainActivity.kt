f2aeaed1f28165c24dc7177ae6056d081ae2c3ce
Voxytech <clement.leveque.147@gmail.com>
2021-04-16 22:06:35 +0200

    modif des boutons

--- app/src/main/java/com/eseo/silverguide/ui/MainActivity.kt
+++ app/src/main/java/com/eseo/silverguide/ui/MainActivity.kt
@@ -5,8 +5,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.eseo.silverguide.R
+import com.eseo.silverguide.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding // <-- Référence à notre ViewBinding
 
     companion object {
         fun getStartIntent(context: Context): Intent {
@@ -18,6 +21,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        // NOUVELLE METHODE --> Indique que l'on utilise le ViewBinding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
