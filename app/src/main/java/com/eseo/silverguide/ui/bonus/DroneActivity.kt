a029d7ac6f9ff19decaf61bab473b548ec856429
Voxytech <clement.leveque.147@gmail.com>
2021-04-19 21:51:53 +0200

    amélioration du bonus

--- app/src/main/java/com/eseo/silverguide/ui/bonus/DroneActivity.kt
+++ app/src/main/java/com/eseo/silverguide/ui/bonus/DroneActivity.kt
@@ -36,14 +36,23 @@ class DroneActivity : AppCompatActivity() {
         rv.layoutManager = LinearLayoutManager(this)
         rv.addItemDecoration(DividerItemDecoration(rv.context, DividerItemDecoration.VERTICAL))
         rv.adapter = Adapter(arrayOf(
-                DroneItem(getString(R.string.drone_bleu), R.drawable.fond_blanc) {
+                DroneItem(getString(R.string.drone_bleu), R.drawable.drone_bleu) {
                     Toast.makeText(this@DroneActivity, "drone bleu", Toast.LENGTH_SHORT).show()
                 },
-                DroneItem(getString(R.string.drone_rouge), R.drawable.fond_blanc) {
+                DroneItem(getString(R.string.drone_rouge), R.drawable.drone_red) {
                     Toast.makeText(this@DroneActivity, "drone rouge", Toast.LENGTH_SHORT).show()
                 },
-                DroneItem(getString(R.string.drone_3D), R.drawable.fond_blanc) {
+                DroneItem(getString(R.string.drone_3D), R.drawable.drone_3d) {
                     Toast.makeText(this@DroneActivity, "drone imprimé en 3D", Toast.LENGTH_SHORT).show()
+                },
+                DroneItem(getString(R.string.drone_antho), R.drawable.drone_antho) {
+                    Toast.makeText(this@DroneActivity, "drone imprimé en 3D", Toast.LENGTH_SHORT).show()
+                },
+                DroneItem(getString(R.string.drone_pilou), R.drawable.drone_pilou) {
+                    Toast.makeText(this@DroneActivity, "tondeuse de pilou", Toast.LENGTH_SHORT).show()
+                },
+                DroneItem(getString(R.string.drone_tom), R.drawable.drone_tom) {
+                    Toast.makeText(this@DroneActivity, "tondeuse de pilou", Toast.LENGTH_SHORT).show()
                 })){
 
         }
